121d26bca9f4bd25831eff085965e50dcad02f65
sherlock <l17876079248@gmail.com>
2026-06-10 10:18:34 +0800

    mv to uv

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -356,12 +356,12 @@ static void App_PrintAdcSamples(uint32_t count)
     resistance_ohm = (current_abs_a > 0.000001f) ? (voltage_abs_v / current_abs_a) : 0.0f;
 
     BoardUart_Printf(BOARD_UART_PORT_DEBUG,
-                     "adc %lu: Isense=%ld uV I=%ld uA Vraw=%ld mV Vload=%ld mV P=%ld uW R=%ld mOhm st=0x%02X/0x%02X\r\n",
+                     "adc %lu: Isense=%ld uV I=%ld uA Vraw=%ld uV Vload=%ld uV P=%ld uW R=%ld mOhm st=0x%02X/0x%02X\r\n",
                      (unsigned long)(index + 1u),
                      (long)App_FloatToMilli(sample.current_sense_voltage_v * 1000.0f),
                      (long)App_FloatToMilli(sample.current_a * 1000.0f),
-                     (long)App_FloatToMilli(sample.load_voltage_raw_v),
-                     (long)App_FloatToMilli(sample.load_voltage_v),
+                     (long)App_FloatToMilli(sample.load_voltage_raw_v * 1000.0f),
+                     (long)App_FloatToMilli(sample.load_voltage_v * 1000.0f),
                      (long)App_FloatToMilli(power_w * 1000.0f),
                      (long)App_FloatToMilli(resistance_ohm),
                      sample.current_adc_status,
