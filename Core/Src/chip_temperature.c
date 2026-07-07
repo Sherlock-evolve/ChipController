d7f9c9f999ba69785abb855bddfb2f9e4a64d3bf
sherlock <l17876079248@gmail.com>
2026-07-07 12:02:10 +0800

    TCR

--- Core/Src/chip_temperature.c
+++ Core/Src/chip_temperature.c
@@ -18,9 +18,9 @@
  *     R = R0 * (1 + alpha * (T - T0))
  * The values below are placeholders so the project compiles; reported
  * temperatures will be wrong until real R0/alpha/T0 are supplied. */
-#define CHIP_TEMPERATURE_R0_OHM        10.0f    /* resistance at T0 (ohms)       */
-#define CHIP_TEMPERATURE_ALPHA_PER_C   0.001f    /* temperature coefficient (1/C) */
-#define CHIP_TEMPERATURE_T0_C          25.0f     /* reference temperature (C)     */
+#define CHIP_TEMPERATURE_R0_OHM        37.33644f    /* resistance at T0 (ohms)       */
+#define CHIP_TEMPERATURE_ALPHA_PER_C   0.00206754f    /* temperature coefficient (1/C) */
+#define CHIP_TEMPERATURE_T0_C          20.0f     /* reference temperature (C)     */
 
 /* Resistance at or below this is treated as "not measurable" (no current). */
 #define CHIP_TEMPERATURE_MIN_RESISTANCE_OHM  0.001f
